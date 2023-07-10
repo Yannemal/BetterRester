7528cf4636deeeee75ee5aec90636de7acc57d97
yannemal <56878180+Yannemal@users.noreply.github.com>
2023-07-10 12:17:27 +0800

    basic v1 layout SwiftUI complete
    Text, Steppers, DatePickers in place.
    
    TODO: make it look nicer

--- BetterRester/ContentView.swift
+++ BetterRester/ContentView.swift
@@ -1,25 +1,49 @@
 //
 //  ContentView.swift
 //  BetterRester
-//  DAY26 / 100DaysOfSwiftUI @TwoStraws PaulHudson
-//  Created by yannemal on 09/07/2023.
+//  DAY26 / 100DaysOfSwiftUI by @TwoStraws PaulHudson
+//  Student: yannemal on 09/07/2023.
 //
 
 import SwiftUI
 
 struct ContentView: View {
-    
+    // MARK: - DATA
+    @State private var wakeUp  = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, Machine Learning!")
+        
+        NavigationView {
+         
+            VStack {
+                Text("When do you want to wake up ?")
+                    .font(.headline)
+                
+                DatePicker("Please enter a time",
+                           selection: $wakeUp,
+                           displayedComponents: .hourAndMinute)
+                .labelsHidden()
+                
+                Text("desired amount of sleep")
+                    .font(.headline)
+                
+                Stepper("\(sleepAmount.formatted()) hrs",
+                value: $sleepAmount,
+                        in: 4...12,
+                        step: 0.25)
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                
+                
+            }
         }
-        .padding()
-    }
-}
+        
+    } // end body View
+    
+} // end contentView
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
